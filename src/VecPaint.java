95ef76356d943c9e8e63a9168d1e13a5e38c9ba9
Davina <davina.t@hotmail.com>
2019-05-09 13:52:21 +1000

    added panels in borderLayout

--- src/VecPaint.java
+++ src/VecPaint.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
-import javax.tools.Tool;
 import java.awt.*;
+import javax.swing.border.Border;
+import javax.tools.Tool;
 
 public class VecPaint extends JFrame{
     private int screenWidth;
@@ -14,7 +15,10 @@ public class VecPaint extends JFrame{
     private JMenuItem fileMenuItemOpen = new JMenuItem("Open");
 
     //Panel variables
-    private JPanel dummyPanel;
+    private JPanel pnlCanvas;
+    private JPanel pnlTools;
+    private JPanel pnlColours;
+    private JPanel pnlBottom;
 
     private VecPaint(){
         super("VecPaint tool");
@@ -42,6 +46,7 @@ public class VecPaint extends JFrame{
      * create GUI components
      */
     private void createVecGUI(){
+        setLayout(new BorderLayout());
 
         // menu bar items
         fileMenu.add(fileMenuItemSave);
@@ -50,23 +55,33 @@ public class VecPaint extends JFrame{
         menuBar.add(fileMenu);
         setJMenuBar(menuBar);
 
-        // dummy panel
-        dummyPanel = createPanel(Color.WHITE);
-        super.getContentPane().add(dummyPanel);
+        // panels
+        pnlCanvas = createPanel(Color.WHITE);
+        pnlTools = createPanel(Color.LIGHT_GRAY);
+        pnlColours = createPanel(Color.LIGHT_GRAY);
+        pnlBottom = createPanel(Color.LIGHT_GRAY);
+
+        getContentPane().add(pnlCanvas, BorderLayout.CENTER);
+        getContentPane().add(pnlTools, BorderLayout.WEST);
+        getContentPane().add(pnlColours, BorderLayout.EAST);
+        getContentPane().add(pnlBottom, BorderLayout.SOUTH);
+
+        pnlTools.setPreferredSize(new Dimension(screenWidth / 24, screenHeight));
+        pnlColours.setPreferredSize(new Dimension(screenWidth / 24, screenHeight));
+        pnlBottom.setPreferredSize(new Dimension(screenWidth, screenHeight / 26));
 
         // pack up
-        super.setPreferredSize(new Dimension(screenWidth / 2, screenHeight / 2));
-        super.pack();
-        super.setVisible(true);
+        setPreferredSize(new Dimension(screenWidth / 2, screenHeight / 2));
+
+        pack();
+        repaint();
+        setVisible(true);
     }
-
-
 
     public static void main(String[] args){
         VecPaint vectorTool = new VecPaint();
         vectorTool.getScreenSize();
         vectorTool.createVecGUI();
-
     }
 
 }
