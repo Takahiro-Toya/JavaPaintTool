94b15b563ee7ea768b21be6439f49dbd449a17de
Yichuan Wang <w369371@gmail.com>
2019-05-12 11:27:40 +1000

    finished the save file function and open file function, now it can output a .vec file and save it, read contents from a local .vec file

--- src/VecFileManager.java
+++ src/VecFileManager.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.ArrayList;
 
 public class VecFileManager extends JMenuItem {
+    /**
+     * Enums names of every component of the menu bar
+     */
     public enum MenuNames{
         File("File"), Undo("Undo"), Save("Save"), New("New"), Open("Open");
         private final String name;
@@ -12,19 +18,31 @@ public class VecFileManager extends JMenuItem {
             name = s;
         }
     }
+    // Variables
     private File vecFile;
-    String fileName;
+    private ArrayList Savelist = new ArrayList();
+    private ArrayList Openlist = new ArrayList();
 
 
-
+    /**
+     * The constructor
+     * @param name the name of the component
+     */
     public VecFileManager(String name) {
         super(name);
     }
 
+    /**
+     * Get the current file
+     */
     public File getFile(){
         return vecFile;
     }
 
+    /**
+     * Create a Jmenu bar and add all the component into it
+     * @return return the completed menu bar
+     */
     public JMenuBar createJmenu(){
         JMenuBar bar = new JMenuBar();
 
@@ -51,30 +69,56 @@ public class VecFileManager extends JMenuItem {
         return bar;
     }
 
-
-
-
+    /**
+     * Save the current file into the chosen directory
+     * @return return the finished listener
+     */
     public ActionListener getSaveListener(){
-        JFileChooser chooser = new JFileChooser();
-        ActionListener listener = new ActionListener() {
+       ActionListener saveListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.addChoosableFileFilter(new FileFilter() {
+                    @Override
+                    public boolean accept(File f) {
+                        if (f.getName().endsWith(".vec")){
+                            return true;
+                        }else{
+                            return f.getName().toLowerCase().endsWith(".vec");
+                        }
+                    }
+                    @Override
+                    public String getDescription() {
+                        return "VEC Documents (*.vec)";
+                    }
+                });
                 if (chooser.showSaveDialog(new Label()) == JFileChooser.APPROVE_OPTION) {
                     vecFile = chooser.getSelectedFile();
-                    String name = chooser.getName(vecFile);
-                    vecFile = new File(chooser.getCurrentDirectory(), name + ".txt");
+                    String path = vecFile.getPath();
                     try{
+                        if (!path.toLowerCase().endsWith(".vec")){
+                            vecFile = new File(path + ".vec");
+                        }
                         FileOutputStream fos = new FileOutputStream(vecFile);
                         fos.close();
-                    }catch (IOException ex){
-                        ex.printStackTrace();
+                        FileWriter fileWriter = new FileWriter(vecFile);
+                        ////
+                        fileWriter.write("\n");
+                        fileWriter.close();
+                        String name = chooser.getName(vecFile);
+
+
+                    }catch (IOException es){
+                         es.printStackTrace();
                     }
                 }
+
             }
         };
-        return listener;
+        return saveListener;
     }
 
+    // havent finished
     public ActionListener getNewListener(){
         return new ActionListener() {
             @Override
@@ -84,18 +128,31 @@ public class VecFileManager extends JMenuItem {
         };
     }
 
+    /**
+     * Load the content of the chosen file into a string
+     * @return return this finished listener
+     */
     public ActionListener getOpenListener(){
         java.awt.event.ActionListener listener = new ActionListener() {
             JFileChooser chooser = new JFileChooser();
             public void actionPerformed(ActionEvent e) {
                 if (chooser.showOpenDialog(new Label()) == JFileChooser.APPROVE_OPTION) {
                     vecFile = chooser.getSelectedFile();
-                    fileName = chooser.getName(vecFile);
                     try {
                         InputStreamReader reader = new InputStreamReader(new FileInputStream(vecFile));
                         BufferedReader br = new BufferedReader(reader);
-                    } catch (FileNotFoundException e1) {
+                        String text = null;
+                        while ((text = br.readLine()) != null){
+                            Openlist.add(text);
+                        }
+                        // test
+                        System.out.println(Openlist);
+                        // test
+                        br.close();
+                        reader.close();
 
+                    } catch (Exception e1) {
+                       e1.printStackTrace();
                     }
                 }
             }
