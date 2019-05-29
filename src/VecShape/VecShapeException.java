760c0bafd3d329cf154448dc08da0bf40fb4fbcf
TakahiroToya <taka0122milk@gmail.com>
2019-05-29 18:56:33 +1000

    commenting

--- src/VecShape/VecShapeException.java
+++ src/VecShape/VecShapeException.java
@@ -1,7 +1,14 @@
 package VecShape;
 
+/**
+ * An exception to be thrown when failing creating an VecShape object
+ */
 public class VecShapeException extends RuntimeException{
 
+    /**
+     * Specify message of the exception
+     * @param message message that you want to show
+     */
     public VecShapeException(String message){
         super(message);
     }
